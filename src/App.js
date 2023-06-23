c90a380fc42828d5d8d6b5bf7a5b4d85b3f4b69c
seandhoey <seanhoey@live.ca>
2023-06-23 16:44:29 -0600

    Clarifai API returns a value. Skeleton of input and button functional

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 // TODO May not need App.css
 import './App.css';
 // TODO Consider: https://stackoverflow.com/a/29722646
@@ -8,22 +9,76 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition.js'
 import Rank from './components/Rank/Rank.js'
 
 
-function App() {
-  return (
-    <div>
-      <header className='flex'>
-        <Logo />
-        <div style={{ flexGrow: '1' }} />
-        <Navigation />
-      </header>
-      <section>
-        <div style={{ height: '10vh' }} />
-        <Rank />
-        <ImageLinkForm />
-        <FaceRecognition />
-      </section>
-    </div>
-  );
+class App extends React.Component {
+
+  // App State constructor
+  constructor() {
+    super()
+    this.state = {
+      imgURL: ''
+    }
+  }
+
+  // Given a URL and the static config, generates a requestOptions object, used in the fetch API
+  generateClarifaiRequest(url) {
+    const PAT_KEY = '450fdb8a850148248a9815d0e41dc6ae';
+    const USER_ID = 'nmvision';
+    const APP_ID = 'FaceDetection';
+
+    return {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT_KEY
+      },
+      body: JSON.stringify({
+        "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+        },
+        "inputs": [{
+          "data": { "image": { "url": url } }
+        }]
+      })
+    };
+  }
+
+  onInputChange = (event) => {
+    // The second arg to setState executes once the asynchronous first part has completed
+    this.setState({imgURL: event.target.value} , () => console.log(this.state.imgURL));
+  }
+
+  onSubmit = () => {
+    if(this.state.imgURL !== ""){
+      console.log('click');
+      // TODO nest the below inside and assign actual imgURL
+    }
+
+    var url = 'https://samples.clarifai.com/metro-north.jpg';
+
+    fetch("https://api.clarifai.com/v2/models/face-detection/outputs", this.generateClarifaiRequest(url))
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
+
+  render() {
+    return (
+      <div>
+        <header className='flex'>
+          <Logo />
+          <div style={{ flexGrow: '1' }} />
+          <Navigation />
+        </header>
+        <section>
+          <div style={{ height: '10vh' }} />
+          <Rank />
+          <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
+          <FaceRecognition />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './imageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onSubmit }) => {
   return (
     <div className='mw5 mw7-ns center tc'>
       <p className='f3 black'>
@@ -9,8 +9,13 @@ const ImageLinkForm = () => {
       </p>
       <div className='pa3 ph4-ns pv4-ns br4 carbon'>
         <div className='flex dib'>
-          <input className='f4 w-70 mr2 br2' type='text'></input>
-          <button className='w-30 f4 pv2 br2 black bg-washed-green hover-bg-light-blue pointer'>
+          <input
+            onChange={onInputChange}
+            className='f4 w-70 mr2 br2'
+            type='text' />
+          <button
+            onClick={onSubmit}
+            className='w-30 f4 pv2 br2 black bg-washed-green hover-bg-light-blue pointer'>
             Detect
           </button>
         </div>
